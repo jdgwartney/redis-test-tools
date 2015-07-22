571a1d9d6f32f4433a7330a17c27c3bfb71840fb
jdgwartney <davidg@boundary.com>
2015-07-22 09:06:50 -0700

    Check

---
+++ src/main/java/com/boundary/redis/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.boundary.redis;
+
+import redis.clients.jedis.*;
+
+public class HelloWorld {
+    private JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost");
+
+    private void test() {
+        try
+        {
+            Jedis jedis = pool.getResource();
+            jedis.set("MSG", "Hello World");
+            String result = jedis.get("MSG");
+            System.out.println(" MSG : " + result);
+            pool.returnResource(jedis);
+
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.toString());
+        }finally{
+            pool.destroy();
+        }
+
+    }
+
+    public static void main(String args[])
+    {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.test();
+    }
+
+}
